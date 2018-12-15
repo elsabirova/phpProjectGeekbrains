3bff9172716bb731c538074e1aeb5b6afefb1f5a
elsabirova <grow_happy@mail.ru>
2018-12-15 01:26:33 +0100

    autoload using composer

--- public/index.php
+++ public/index.php
@@ -1,9 +1,6 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT'] . "/../config/config.php";
-include ROOT_DIR . "services/Autoloader.php";
 require_once ROOT_DIR . "vendor/autoload.php";
-
-spl_autoload_register([new \app\services\Autoloader(), 'loadClass']);
 
 $controllerName = $_GET['c']?:DEFAULT_CONTROLLER;
 $actionName = $_GET['a'];
