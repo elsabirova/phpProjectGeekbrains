dde26ba1a859e67036aa40dc4c3c3e23f231e124
elsabirova <grow_happy@mail.ru>
2018-12-15 02:51:50 +0100

    make names in catalog like link

--- views/good/catalog.php
+++ views/good/catalog.php
@@ -1,9 +1,9 @@
 <h1>Catalog</h1>
-<p>
-    <? foreach ($goods as $good) { ?>
-        <h3><?=$good->name?></h3>
+<? foreach ($goods as $good): ?>
+    <div>
+        <a href="/?c=good&a=card&id=<?=$good->id?>"><?=$good->name?></a>
         <p><?=$good->description?></p>
         <p><?=$good->price?></p>
         <button>Buy</button>
-    <?  } ?>
-</p>
+    </div>
+<? endforeach; ?>
